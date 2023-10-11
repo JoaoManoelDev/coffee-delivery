b1a7e3d61470ffe01b9bd1f1e0a6a193d7eab368
João Manoel <joaomanoeldev@gmail.com>
2023-10-11 18:14:23 -0300

    chore: remove background into

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -4,7 +4,7 @@ import { OurCoffees } from "./components/our-coffees"
 export default function Home() {
   return (
     <div className="mt-[5.5rem] space-y-5">
-      <div className="bg-[url('/intro-background.png')] bg-no-repeat bg-cover bg-center">
+      <div>
         <Hero />
       </div>
 
